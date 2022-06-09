b9c46885578de168603ddcd7ab1d754fc4bd8045
NachoCarher <nachoch64@correo.ugr.es>
2022-06-10 00:45:23 +0200

    Se añade plugin para tratar con imágenes

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,12 +1,18 @@
 // Step 1: Import React
 import * as React from 'react'
 import Layout from './layout'
+import { StaticImage } from 'gatsby-plugin-image'
+
 
 // Step 2: Define your component
 const IndexPage = () => {
   return (
     <Layout pageTitle="Home Page">
       <p>I'm making this by following the Gatsby Tutorial.</p>
+      <StaticImage
+        alt="Coche tesla"
+        src="../images/modelo_3.png"
+      />
     </Layout>
   )
 }
